e65a1b37d21b4083c26c96189a499d658bca2620
defendi.andrea <defendi.andrea@BIBLIO-PC04.OSDBSESTO.local>
2020-01-20 13:03:19 +0100

    Add infinite loop

--- es 1/src/es/pkg1/Es1.java
+++ es 1/src/es/pkg1/Es1.java
@@ -15,7 +15,9 @@ public class Es1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("prova");
+        for (;;) {
+            System.out.println("prova");
+        }
     }
     
 }
